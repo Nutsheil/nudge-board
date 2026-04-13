3b761416e31bc751dab35256907f60042fba4578
Redchenko Evgeniy <nutshell2088@yandex.ru>
2026-04-13 21:12:27 +0300

    Connect Prisma and PostgreSQL

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 3000;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableShutdownHooks();
+
+  await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 }
-bootstrap();
+void bootstrap();
